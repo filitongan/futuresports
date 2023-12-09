50d87a8db1a8e54d1ceff25b37b71571f4afae88
filitongan <152054181+filitongan@users.noreply.github.com>
2023-12-10 07:31:55 +1100

    Add error boundary to app

--- src/App.js
+++ src/App.js
@@ -13,26 +13,46 @@ import TermsAndConditions from "./pages/TermsAndConditions";
 import Faq from "./pages/Faq";
 import Fanzone from "./pages/Fanzone";
 import ContactUs from "./pages/ContactUs";
+import { ErrorBoundary } from "react-error-boundary";
+
+function MyFallbackComponent({ error, resetErrorBoundary }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Team" element={<Team />} />
-        <Route path="/Events" element={<Events />} />
-        <Route path="/News" element={<News />} />
-        <Route path="/Fanzone" element={<Fanzone />} />
-        <Route path="/NewsArticle" element={<NewsArticle />} />
-        <Route path="/Video" element={<Video />} />
-        <Route path="/PrivacyStatement" element={<PrivacyStatement />} />
-        <Route path="/TermsAndConditions" element={<TermsAndConditions />} />
-        <Route path="/Faq" element={<Faq />} />
-        <Route path="/ContactUs" element={<ContactUs />} />
-      </Routes>
+      <ErrorBoundary
+        FallbackComponent={MyFallbackComponent}
+        onError={(error, errorInfo) => console.log({ error, errorInfo })}
+        onReset={() => {
+          // reset the state of your app
+          window.location.reload();
+        }}
+      >
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Team" element={<Team />} />
+          <Route path="/Events" element={<Events />} />
+          <Route path="/News" element={<News />} />
+          <Route path="/Fanzone" element={<Fanzone />} />
+          <Route path="/NewsArticle" element={<NewsArticle />} />
+          <Route path="/Video" element={<Video />} />
+          <Route path="/PrivacyStatement" element={<PrivacyStatement />} />
+          <Route path="/TermsAndConditions" element={<TermsAndConditions />} />
+          <Route path="/Faq" element={<Faq />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+        </Routes>
 
-      <Footer />
+        <Footer />
+      </ErrorBoundary>
     </>
   );
 }
