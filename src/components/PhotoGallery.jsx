60f75cc794356fe1b4636b12237343e7b8a7b497
filitongan <152054181+filitongan@users.noreply.github.com>
2023-12-10 13:46:41 +1100

    Add photo gallery component

---
+++ src/components/PhotoGallery.jsx
@@ -0,0 +1,22 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+import homepageBanner from "../assets/images/homepage/homepage_banner_img_1.png";
+import goalPostBanner from "../assets/images/banners/goal_post_banner.png";
+import newsBanner from "../assets/images/banners/news_banner.png";
+
+export default function PhotoGallery() {
+  const images = [homepageBanner, goalPostBanner, newsBanner];
+  return (
+    <div className="photoGallery" id="PhotoGallery">
+      <h3 className="section-title">Photo Gallery</h3>
+      <Carousel>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img src={image} alt={`Banner ${index}`} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,115 +1,25 @@
 import "../css/Home.css";
-import homepageBanner from "../assets/images/homepage/homepage_banner_img_1.png";
-import goalPostBanner from "../assets/images/banners/goal_post_banner.png";
-import newsBanner from "../assets/images/banners/news_banner.png";
-
 import game1 from "../assets/images/homepage/game_1.png";
 import game2 from "../assets/images/homepage/game_2.png";
 import game3 from "../assets/images/homepage/game_3.png";
 import game4 from "../assets/images/homepage/game_4.png";
-
 import news1 from "../assets/images/homepage/news_1.png";
 import news2 from "../assets/images/homepage/news_2.png";
 import news3 from "../assets/images/homepage/news_3.png";
 import news4 from "../assets/images/homepage/news_4.png";
-
 import video1 from "../assets/images/homepage/video_1.png";
 import video2 from "../assets/images/homepage/video_2.png";
 import video3 from "../assets/images/homepage/video_3.png";
 import video4 from "../assets/images/homepage/video_4.png";
-
 import rightArrow from "../assets/images/homepage/right_arrow.png";
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import PhotoGallery from "../components/PhotoGallery";
 
 export default function Home() {
-  const [slideIndex, setSlideIndex] = useState(1);
-
-  function plusSlides(n) {
-    if (slideIndex + n > 3) {
-      setSlideIndex(1);
-    } else if (slideIndex + n < 1) {
-      setSlideIndex(3);
-    } else {
-      setSlideIndex((prevIndex) => prevIndex + n);
-    }
-  }
-  function currentSlide(n) {
-    setSlideIndex(n);
-  }
-
-  useEffect(() => {
-    function showSlides(n) {
-      let i;
-      let slides = document.getElementsByClassName("mySlides");
-      let dots = document.getElementsByClassName("dot");
-
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-
-      slides[slideIndex - 1].style.display = "block";
-      dots[slideIndex - 1].className += " active";
-    }
-    showSlides(slideIndex);
-  }, [slideIndex]);
-
   return (
     <div className="main-content">
       <div className="banner">
-        <div className="slideshow-container">
-          <div className="mySlides fade">
-            <img src={homepageBanner} alt="homepage banner" />
-          </div>
-          <div className="mySlides fade">
-            <img src={goalPostBanner} alt="goal post banner" />
-          </div>
-
-          <div className="mySlides fade">
-            <img src={newsBanner} alt="news banner" />
-          </div>
-
-          <button
-            className="arrow left-arrow"
-            onClick={() => {
-              plusSlides(-1);
-            }}
-          >
-            &#10094;
-          </button>
-          <button
-            className="arrow right-arrow"
-            onClick={() => {
-              plusSlides(1);
-            }}
-          >
-            &#10095;
-          </button>
-
-          <div className="dots">
-            <span
-              className="dot"
-              onClick={() => {
-                currentSlide(1);
-              }}
-            ></span>
-            <span
-              className="dot"
-              onClick={() => {
-                currentSlide(2);
-              }}
-            ></span>
-            <span
-              className="dot"
-              onClick={() => {
-                currentSlide(3);
-              }}
-            ></span>
-          </div>
-        </div>
+        <PhotoGallery />
       </div>
 
       <div className="upcoming-matches">
